8ccf69a36ea4dd61fb42be967305e86349d4f11f
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2022-10-16 22:11:58 +0200

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "http://dhaneshnm.github.io",
-  base: "/personal-website",
   integrations: [mdx(), sitemap()],
 });
